121d219b78d85e6b93a64da2ac8f0702e9f6ffb5
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-11 23:43:07 -0500

    added constructor

--- Car.cs
+++ Car.cs
@@ -2,6 +2,15 @@ using System;
 
 class Car
 {
+    private string name;
+    public Car (string name)
+    {
+        this.name = name; 
+    }
+    public string GetName()
+    {
+        return name; 
+    }
     public void Start()
     {
         Console.WriteLine("The car has started");
